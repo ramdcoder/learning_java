9ebcf3519498776158a4cad339dea36e477a9dfc
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-03 07:12:42 -0300

    Hello Java program with GUI

---
+++ HelloJava.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class HelloJava {
+
+    public static void main( String[] args ) {
+
+        JFrame frame = new JFrame( "Hello, Java!" );
+        JLabel label = new JLabel( "Hello, Java!", JLabel.CENTER );
+
+        frame.add(label);
+
+        frame.setSize( 300, 300 );
+        frame.setVisible( true );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+    }
+
+}
