be02c54fa386270f9e253e9d52a011672887f88d
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-05 22:01:49 -0300

    practicing mathematical operations in Java. Difference between print and println

---
+++ BasicsDemo.java
@@ -0,0 +1,22 @@
+class BasicsDemo {
+
+  public static void main(String[] args) {
+
+    int i = 6;
+    int j = 2;
+
+    System.out.println("Some math to practice Java!");
+    System.out.print("i + j = ");
+    System.out.println(i + j);
+    System.out.print("i - j = ");
+    System.out.println(i - j);
+    System.out.print("i * j = ");
+    System.out.println(i * j);
+    System.out.print("i / j = ");
+    System.out.println(i / j);
+    System.out.print("i % j = ");
+    System.out.println(i % j);
+    System.out.println("% means the remainder of the division. In this case it is 0.");
+
+  }
+}
