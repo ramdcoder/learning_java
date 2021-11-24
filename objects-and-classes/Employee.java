b7f41d21ce40b0ff0bcc7bf0995d9972257e7ea0
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-24 06:56:36 -0300

    implementation of a class Employee with methods to get the name, salary and hire day

---
+++ objects-and-classes/Employee.java
@@ -0,0 +1,40 @@
+import java.time.*;
+
+class Employee
+{
+    /**
+    * Criação de classe para praticar.
+    * @version 0.1 2021-11-24
+    **/
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String n, double s, int year, int month, int day)
+    {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public LocalDate getHireDay()
+    {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        double raise = salary * byPercent/100;
+        salary += raise;
+    }
+}
